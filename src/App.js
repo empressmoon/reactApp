8a9692e8c2c6c57593326a01af384c154b31116b
Karolina Gorlewska <k.gorlewska@gmail.com>
2019-07-14 13:37:28 +0100

    Add form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,59 @@
 import React from 'react';
 import ListWrapper from './components/ListWrapper/ListWrapper';
 import './index.css';
+import Form from './components/Form/Form';
 
-const App = () => (
-  <div>
-    <ListWrapper />
-  </div>
-);
+const initialStateItems = [
+  {
+    image: 'https://i.kym-cdn.com/entries/icons/mobile/000/013/564/doge.jpg',
+    name: 'Dan Abramov',
+    description: 'Working on @reactjs. The demo guy.',
+    twitterLink: 'https://twitter.com/dan_abramov'
+  },
+  {
+    image: 'https://i.kym-cdn.com/entries/icons/mobile/000/013/564/doge.jpg',
+    name: 'Ryan Florence',
+    description:
+      'Making React accessible for users and developers at https://reach.tech . Online learning, workshops, OSS, and consulting.',
+    twitterLink: 'https://twitter.com/ryanflorence'
+  },
+  {
+    image: 'https://i.kym-cdn.com/entries/icons/mobile/000/013/564/doge.jpg',
+    name: 'Michael Jackson',
+    description:
+      'Maker. Co-author of React Router. Working on @ReactTraining. Created @unpkg. Head over heels for @cari.',
+    twitterLink: 'https://twitter.com/mjackson'
+  },
+  {
+    image: 'https://i.kym-cdn.com/entries/icons/mobile/000/013/564/doge.jpg',
+    name: 'Kent C. Dodds',
+    description:
+      'Making software development more accessible · Husband, Father, Latter-day Saint, Teacher, OSS, GDE, @TC39 · @PayPalEng @eggheadio @FrontendMasters · #JS',
+    twitterLink: 'https://twitter.com/kentcdodds'
+  }
+];
+
+class App extends React.Component {
+  state = {
+    items: [...initialStateItems]
+  };
+
+  addItem = e => {
+    e.preventDefault();
+    console.log(e.target[0].value);
+    console.log(e.target[1].value);
+    console.log(e.target[2].value);
+    console.log(e.target[3].value);
+  };
+
+  render() {
+    return (
+      <div>
+        <ListWrapper items={this.state.items} />
+        <Form submitFn={this.addItem} />
+      </div>
+    );
+  }
+}
 
 export default App;
